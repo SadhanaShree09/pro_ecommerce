67670cd4046110c8b164f1f15bd9e0995f6b8f16
Muthu39 <bmkmuthu003@gmail.com>
2025-04-02 22:52:54 +0530

    Feat : order code is coded

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const products = require('./routes/product');
+const orders = require('./routes/order');
+app.use('/api/v1',products);
+app.use('/api/v1',orders);
+
 dotenv.config({path: path.join(__dirname,'config' , 'config.env')})
 app.listen(process.env.PORT,() =>{
     console.log(`server listening in port ${process.env.PORT} in ${process.env.NODE_ENV}`)
---
+++ backend/routes/order.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { getOrder } = require('../controllers/orderController');
+const { route } = require('./product');
+const router = express.Router();
+router.route('order/').post(getOrder);
+module.exports = router;
