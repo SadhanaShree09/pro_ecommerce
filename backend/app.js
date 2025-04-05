45427ba5ad766c0ca3f05012fa244f6e6d58f4c6
Muthu39 <bmkmuthu003@gmail.com>
2025-04-05 18:16:06 +0530

    feat:added files upto search navigation

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,15 @@ const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
 const connectDatabase = require('./config/connectDatabase');
+const cors = require('cors');
 dotenv.config({path: path.join(__dirname,'config' , 'config.env')})
 
 const products = require('./routes/product');
 const orders = require('./routes/order');
 
 connectDatabase();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import Home from './pages/Home';
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+        <Header/>
+        <Routes>
+          <Route path="/" element = { <Home />}/>
+        </Routes>
+        </div>
+      </Router>
+      
+     
+      <Footer/>
     </div>
   );
 }
