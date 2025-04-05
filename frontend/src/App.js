2a82afce2d69abe22ec85d613f76ca263d53f634
Muthu39 <bmkmuthu003@gmail.com>
2025-04-05 23:38:26 +0530

    Feat : Added the product details functionalities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import ProductDetail from './pages/ProductDetail';
 function App() {
   return (
     <div className="App">
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element = { <Home />}/>
           <Route path="/search" element = { <Home />}/>
-
+          <Route path="/product/:id" element = { <ProductDetail />}/>
         </Routes>
         </div>
       </Router>
