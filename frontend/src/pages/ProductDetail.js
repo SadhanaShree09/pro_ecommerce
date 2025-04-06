b0e9e536989c805237fc8a07d7daeb88b6bb424e
Muthu39 <bmkmuthu003@gmail.com>
2025-04-06 17:57:54 +0530

    Feat : Completed E-Commerce Website successfully

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,14 +1,39 @@
 import { useState ,useEffect} from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function ProductDetail(){
+export default function ProductDetail({cartItems,setCartItems}){
     const [product,setProduct]= useState(null);
+    const [qty,setQty] = useState(1);
     const {id}=useParams();
     useEffect(()=> {
           fetch(process.env.REACT_APP_API_URL+'/products/'+id)
           .then(res=>res.json())
           .then (res=>setProduct(res.product))
         },[])
+
+        function addToCart(){
+            const itemExist = cartItems.find((item)=>item.product._id == product._id)
+            if(!itemExist) {
+             const newItem = {product, qty};
+             setCartItems ((state)=>[...state,newItem]);
+             toast.success("Cart Item Added Successfully!");
+            }
+        }
+
+        function increaseQty(){
+            if (product.stock == qty){
+                return;
+            }
+            setQty((state)=> state+1);
+        }
+
+        function decreaseQty(){
+            if (qty > 1){
+             setQty((state)=> state-1);
+            }
+            
+        }
 
     return product && <div className="container container-fluid">
     <div className="row f-flex justify-content-around">
@@ -31,13 +56,13 @@ export default function ProductDetail(){
 
             <p id="product_price">₹{product.price}</p>
             <div className="stockCounter d-inline">
-                <span className="btn btn-danger minus">-</span>
+                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
-                <input type="number" className="form-control count d-inline" value="1" readOnly />
+                <input type="number" className="form-control count d-inline" value={qty} readOnly />
 
-                <span className="btn btn-primary plus">+</span>
+                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
             </div>
-             <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
+             <button type="button" onClick={addToCart} disabled={product.stock == 0} id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
 
             <hr/>
 
